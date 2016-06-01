30dabd8e01629c158b50b44093688251d453acab
shangzhixin <shangzhixin@sumaplayer.com>
2016-06-01 13:20:24 +0800

    test for function of NSThread

---
+++ TestForThread/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestForThread
+//
+//  Created by new on 16-6-1.
+//  Copyright (c) 2016年 new. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TestForThread/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestForThread
+//
+//  Created by new on 16-6-1.
+//  Copyright (c) 2016年 new. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
